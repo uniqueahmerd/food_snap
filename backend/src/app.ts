ab70c3d9359eafd021377428057b1ac0c7e57891
uniqueahmerd <ahmerdothman@gmail.com>
2026-01-01 18:43:04 +0000

    changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://food-snap-frontend.vercel.app" ,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
