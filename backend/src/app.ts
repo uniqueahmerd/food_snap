cb616478796c0a5d227398d5d8e5083667ee77e3
uniqueahmerd <ahmerdothman@gmail.com>
2026-01-01 18:49:38 +0000

    changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -22,7 +22,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    // origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: '*',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
