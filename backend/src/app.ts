f3384ea05b58ab7ead44f378baa1fc99c9964011
uniqueahmerd <ahmerdothman@gmail.com>
2026-01-08 18:44:16 +0000

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,29 +10,19 @@ import userRouter from "./modules/user/user.routes.js";
 dotenv.config();
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL || "http://localhost:5173", // Vite default
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
-
 app.use(
   cors({
-    // origin: url || "http://localhost:5173" , // Vite default
-    origin: "https://food-snap-backend.vercel.app/",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-// app.use((req, res, next) => {
-//   res.header("Vary", "Origin");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Vary", "Origin");
+  next();
+});
 
 
 app.use(helmet());
